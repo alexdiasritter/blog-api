004b703d6e6a8aedb1a47a09143d6f4ee56d4414
AlexRitter1 <ritter.alex@hotmail.com>
2025-10-13 14:39:51 -0300

    feat (UserRepository): add método findByAuth0Sub

--- src/main/java/com/alex/blog/repository/UserRepository.java
+++ src/main/java/com/alex/blog/repository/UserRepository.java
@@ -3,5 +3,9 @@ package com.alex.blog.repository;
 import com.alex.blog.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByAuth0Sub(String auth0Sub);
+
 }
